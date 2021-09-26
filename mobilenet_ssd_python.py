1a1cf2107ef79a2e025c37399ed9ea6aea04b4de
mohanrajreese <mohanrj1345@gmail.com>
2021-09-26 17:43:46 +0530

    Initial commit

---
+++ mobilenet_ssd_python.py
@@ -0,0 +1,21 @@
+#Import the neccesary libraries
+import numpy as np
+import argparse
+import cv2
+
+#Next, add the parser command lines:
+
+# construct the argument parse
+parser = argparse.ArgumentParser(
+    description='Script to run MobileNet-SSD object detection network ')
+parser.add_argument("--video", help="path to video file. If empty, camera's stream will be used")
+parser.add_argument("--prototxt", default="MobileNetSSD_deploy.prototxt",
+                                  help='Path to text network file: '
+                                       'MobileNetSSD_deploy.prototxt for Caffe model or '
+                                       )
+parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
+                                 help='Path to weights: '
+                                      'MobileNetSSD_deploy.caffemodel for Caffe model or '
+                                      )
+parser.add_argument("--thr", default=0.2, type=float, help="confidence threshold to filter out weak detections")
+args = parser.parse_args()
