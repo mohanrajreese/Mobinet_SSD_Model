35e1edff7aaed0075402b60f4d2cbd5fb32a56fd
mohanrajreese <mohanrj1345@gmail.com>
2021-09-26 17:47:55 +0530

    update

--- mobilenet_ssd_python.py
+++ mobilenet_ssd_python.py
@@ -19,3 +19,26 @@ parser.add_argument("--weights", default="MobileNetSSD_deploy.caffemodel",
                                       )
 parser.add_argument("--thr", default=0.2, type=float, help="confidence threshold to filter out weak detections")
 args = parser.parse_args()
+
+#Labels of network.
+classNames = { 0: 'background',
+    1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat',
+    5: 'bottle', 6: 'bus', 7: 'car', 8: 'cat', 9: 'chair',
+    10: 'cow', 11: 'diningtable', 12: 'dog', 13: 'horse',
+    14: 'motorbike', 15: 'person', 16: 'pottedplant',
+    17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor' }
+
+# Next, open the video file or capture device depending what we choose, also load the model Caffe model.
+# Open video file or capture device.
+if args.video:
+    cap = cv2.VideoCapture(args.video)
+else:
+    cap = cv2.VideoCapture(0)
+
+#Load the Caffe model
+net = cv2.dnn.readNetFromCaffe(args.prototxt, args.weights)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    frame_resized = cv2.resize(frame,(300,300)) # resize frame for prediction
